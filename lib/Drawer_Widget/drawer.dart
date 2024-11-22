a9f8f6e60b9c1c7e9ad57befcd32bba6ace2242b
Choun Piseth <int3072@I101003003.local>
2024-11-22 17:23:00 +0700

    master

--- lib/Drawer_Widget/drawer.dart
+++ lib/Drawer_Widget/drawer.dart
@@ -8,15 +8,72 @@ class CustomDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         padding: const EdgeInsets.all(0),
-        children: const [
+        children: [
           DrawerHeader(
             decoration: BoxDecoration(
-              color: Colors.green,
+              color: Colors.green[50],
             ),
             child: UserAccountsDrawerHeader(
-                accountName: Text('Vencenzo Casano'),
-                accountEmail: Text('vencenzo@gmail.com'),),
-          )
+              decoration: BoxDecoration(color: Colors.green[50]),
+              accountName: const Text(
+                'Vencenzo Casano',
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+              accountEmail: const Text(
+                'vencenzo@gmail.com',
+                style: TextStyle(color: Colors.black),
+              ),
+              currentAccountPictureSize: const Size.square(50),
+              currentAccountPicture: const CircleAvatar(
+                backgroundColor: Colors.black,
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 30.0, color: Colors.blue),
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('My Profile'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+              leading: const Icon(Icons.book),
+              title: const Text('My Course'),
+              onTap: () {
+                Navigator.pop(context);
+              }),
+          ListTile(
+            leading: const Icon(Icons.workspace_premium),
+            title: const Text('Go Premium'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.video_label),
+            title: const Text('Saved Videos'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.edit),
+            title: const Text('Edit Profile'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Log out'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
         ],
       ),
     );
