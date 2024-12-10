8568d00ae2ca01b1bbd965ca05a04e7690b59d11
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-10 17:15:55 +0700

    utils

---
+++ lib/utils/util.dart
@@ -0,0 +1,8 @@
+// Month of Year
+
+class MyDateUtils {
+  static const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+}
