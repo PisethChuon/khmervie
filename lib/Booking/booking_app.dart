9b6764bafd6431485cf7bcc192d7f9cd9a66857c
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-16 16:19:54 +0700

    Booking

--- lib/Booking/booking_app.dart
+++ lib/Booking/booking_app.dart
@@ -18,11 +18,14 @@ class _BookingAppState extends State<BookingApp> {
   final List<String> days = ['Sun', 'Mon', 'Tue', 'Wed', 'Th', 'Fri', 'Sat'];
   final List<String> dates = ['01', '02', '03', '04', '05', '06', '07', '08'];
 
+  int isSelectedDayIndex = 2;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Appointment'),
+        title: Text('Apointment'),
+        centerTitle: true,
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,12 +33,12 @@ class _BookingAppState extends State<BookingApp> {
           _buildDropdownMenu(),
           const SizedBox(height: 16.0),
           _buildCalendar(),
-          const SizedBox(height: 16.0),
+          const SizedBox(height: 8.0),
           _morningSlots(),
-          const SizedBox(height: 16.0),
+          const SizedBox(height: 8.0),
           _afternoonSlots(),
-          const SizedBox(height: 16.0),
-          _everningSlots(),
+          const SizedBox(height: 8.0),
+          _eveningSlots(),
           const SizedBox(height: 16.0),
           _confirmAppointment(),
         ],
@@ -73,35 +76,42 @@ class _BookingAppState extends State<BookingApp> {
         scrollDirection: Axis.horizontal,
         itemCount: days.length,
         itemBuilder: (context, index) {
-          final isSelected = index == 2; // Highlight the first item as default
+          final isSelected = index == isSelectedDayIndex;
 
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 8),
-            width: 60,
-            decoration: BoxDecoration(
-              color: isSelected ? Colors.green : Colors.transparent,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  days[index],
-                  style: TextStyle(
-                    color: isSelected ? Colors.white : Colors.black,
-                    fontWeight: FontWeight.bold,
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                isSelectedDayIndex = index;
+              });
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8),
+              width: 60,
+              decoration: BoxDecoration(
+                color: isSelected ? Colors.green : Colors.transparent,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    days[index],
+                    style: TextStyle(
+                      color: isSelected ? Colors.white : Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  dates[index],
-                  style: TextStyle(
-                    color: isSelected ? Colors.white : Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                  const SizedBox(height: 4),
+                  Text(
+                    dates[index],
+                    style: TextStyle(
+                      color: isSelected ? Colors.white : Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
@@ -109,94 +119,75 @@ class _BookingAppState extends State<BookingApp> {
     );
   }
 
-  // Widget for Morning Slots
+  // Morning Slots
   Widget _morningSlots() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Morning Slots',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(
-            height: 24.0,
-          ),
-          Wrap(
-              spacing: 8.0,
-              runSpacing: 4.0,
-              children: List.generate(5, (index) {
-                return Chip(label: Text('${(index + 1)}:00 AM'));
-              })),
-        ],
-      ),
+    return _slotSection(
+      title: 'Morning Slots',
+      times: List.generate(5, (index) => '${index + 7}:00 AM'),
     );
   }
 
+  // Afternoon Slots
   Widget _afternoonSlots() {
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Afternoon Slots',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 16.0),
-          Wrap(
-              spacing: 8.0,
-              runSpacing: 4.0,
-              children: List.generate(4, (index) {
-                return Chip(label: Text('${(index)}: 00 AM'));
-              }))
-        ],
-      ),
+    return _slotSection(
+      title: 'Afternoo Slots',
+      times: List.generate(5, (index) => '${index + 1}: 00 PM'),
     );
   }
 
-  Widget _everningSlots() {
+  // Evening Slots
+  Widget _eveningSlots() {
+    return _slotSection(
+      title: 'Evening Slots',
+      times: List.generate(3, (index) => '${index + 6}: 00 PM'),
+    );
+  }
+
+  // Slot Section Template
+  Widget _slotSection({required String title, required List<String> times}) {
     return Padding(
-      padding: EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text(
-            'Evening Slots',
-            style: TextStyle(
+          Text(
+            title,
+            style: const TextStyle(
               fontSize: 18.0,
               fontWeight: FontWeight.bold,
             ),
           ),
           const SizedBox(height: 16.0),
           Wrap(
-              spacing: 8.0,
-              runSpacing: 4.0,
-              children: List.generate(4, (index) {
-                return Chip(label: Text('${(index)}: 00 AM'));
-              }))
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: times.map((time) => Chip(label: Text(time))).toList(),
+          ),
         ],
       ),
     );
   }
 
   Widget _confirmAppointment() {
-    return Padding(
-      
-      padding: EdgeInsets.all(16.0),
-      child: Container(
-        child: ElevatedButton(
-          style: ButtonStyle(
-            
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.green, // Set the button color to green
+          padding: const EdgeInsets.symmetric(
+              horizontal: 24.0, vertical: 12.0), // Add padding
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0), // Rounded corners
           ),
-            onPressed: () {}, child: Text('Confirm Appointment')),
+        ),
+        child: const Text(
+          'Confirm Appointment',
+          style: TextStyle(
+            fontSize: 16.0, // Font size
+            fontWeight: FontWeight.bold, // Bold text
+            color: Colors.white, // Text color
+          ),
+        ),
       ),
     );
   }
