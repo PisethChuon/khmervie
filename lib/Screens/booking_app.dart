92097b21e54c9b254148ccf9a5dc6d3484391d65
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-19 10:34:46 +0700

    restructure widget

--- lib/Screens/booking_app.dart
+++ lib/Screens/booking_app.dart
@@ -1,6 +1,7 @@
 import 'package:Khmervie/widgets/calendar_widget.dart';
+import 'package:Khmervie/widgets/confirm_appointment_button.dart';
+import 'package:Khmervie/widgets/slot_section_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class BookingApp extends StatefulWidget {
   const BookingApp({super.key});
@@ -28,88 +29,11 @@ class _BookingAppState extends State<BookingApp> {
                 currentMonth = month;
               });
             }),
-            _morningSlots(),
-            _afternoonSlots(),
-            _eveningSlots(),
-            _confirmAppointment(),
+            const MorningSlots(),
+            const AfternoonSlots(),
+            const EveningSlots(),
+            const ConfirmAppointmentButton(),
           ],
-        ),
-      ),
-    );
-  }
-
-  // Build slot template
-  Widget _slotSection({required String title, required List<String> times}) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 16.0),
-          Wrap(
-            spacing: 8.0,
-            runSpacing: 4.0,
-            children: times.map((time) => Chip(label: Text(time))).toList(),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Morning Slots
-  Widget _morningSlots() {
-    return _slotSection(
-      title: 'Morning Slots',
-      times: List.generate(5, (index) => '${index + 7}:00 AM'),
-    );
-  }
-
-  // Afternoon Slots
-  Widget _afternoonSlots() {
-    return _slotSection(
-      title: 'Afternoon Slots',
-      times: List.generate(5, (index) => '${index + 1}:00 PM'),
-    );
-  }
-
-  // Evening Slots
-  Widget _eveningSlots() {
-    return _slotSection(
-      title: 'Evening Slots',
-      times: List.generate(3, (index) => '${index + 6}:00 PM'),
-    );
-  }
-
-  // Confirm Appointment
-  Widget _confirmAppointment() {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Appointment Confirmation Pressed')),
-          );
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.green,
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        child: const Text(
-          'Confirm Appointment',
-          style: TextStyle(
-            fontSize: 16.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
         ),
       ),
     );
