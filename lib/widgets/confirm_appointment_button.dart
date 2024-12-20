6f6c844843ae861c55217ab8b09bf59937095c26
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-20 17:31:35 +0700

    confirm appointment button

--- lib/widgets/confirm_appointment_button.dart
+++ lib/widgets/confirm_appointment_button.dart
@@ -5,43 +5,19 @@ class ConfirmAppointmentButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //   child: ElevatedButton(
-    //     onPressed: () {
-    //       ScaffoldMessenger.of(context).showSnackBar(
-    //         const SnackBar(content: Text('Appointment Confirmation Pressed')),
-    //       );
-    //     },
-    //     style: ElevatedButton.styleFrom(
-    //       backgroundColor: Colors.green,
-    //       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-    //       shape: RoundedRectangleBorder(
-    //         borderRadius: BorderRadius.circular(8.0),
-    //       ),
-    //     ),
-    //     child: const Text(
-    //       'Confirm Appointment',
-    //       style: TextStyle(
-    //         fontSize: 16.0,
-    //         fontWeight: FontWeight.bold,
-    //         color: Colors.white,
-    //       ),
-    //     ),
-    //   ),
-    // );
     return Center(
       child: ElevatedButton(
         onPressed: () {},
-        child: const Text(
-          'Confirm Appoitment',
-          style: TextStyle(
-              fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.green,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8.0),
           ),
+        ),
+        child: const Text(
+          'Confirm Appoitment',
+          style: TextStyle(
+              fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
     );
