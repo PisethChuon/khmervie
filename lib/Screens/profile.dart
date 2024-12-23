6abab26b50fa4db47cd6418c9e790bdf57d1886e
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-23 10:55:00 +0700

    setting card

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -19,29 +19,35 @@ class Profile extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: const ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage('assets/icons/Khmervie.png'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
               ),
-              title: Text(
-                'Piseth Chuon',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              child: const ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage('assets/icons/Khmervie.png'),
+                ),
+                title: Text(
+                  'Piseth Chuon',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  'Male',
+                  style: TextStyle(fontSize: 16),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+                // onTap: () {
+                //   print('Tapped');
+                // },
               ),
-              subtitle: Text('Male'),
-              trailing: Icon(Icons.arrow_forward_ios),
-              // onTap: () {
-              //   print('Tapped');
-              // },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
