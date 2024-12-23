4b95147ca3ad8be2263537464bdaeb2cce22e135
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-23 17:34:19 +0700

    edit profile

---
+++ lib/widgets/profiles/edit_profile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DateOfBirth extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Date of Birth'),),
+      body: Center (child: Text('Details about Date of Birth'),),
+    );
+  }
+  
+}
