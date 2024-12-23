79fb0c9d8864d82ab9fdf6c6fa0f4b1d59a0d1b3
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-23 13:39:50 +0700

    user profile

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -46,9 +46,68 @@ class Profile extends StatelessWidget {
                 // },
               ),
             ),
+            // Details Info
+            Expanded(
+              child: ListView(
+                children: [
+                  buildDetailItem(
+                    context,
+                    icon: Icons.calendar_today,
+                    label: 'Date of birth',
+                    value: '27/12/2003',
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+Widget buildDetailItem(
+  BuildContext context, {
+  required IconData icon,
+  required String label,
+  required String value,
+}) {
+  return Card(
+    elevation: 2,
+    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(12),
+    ),
+    child: ListTile(
+        leading: CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.blue,
+          child: Icon(icon, color: Colors.white),
+        ),
+        title: Text(
+          label,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              value,
+              style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+              // Icon(Icons.arrow_forward_ios),
+            ),
+            const SizedBox(width: 8),
+            const Icon(
+              Icons.arrow_forward_ios,
+              size: 16,
+              color: Colors.grey,
+            ),
+          ],
+        )
+
+        // onTap: () {
+        //   print('$label tapped');
+        // },
+        ),
+  );
 }
