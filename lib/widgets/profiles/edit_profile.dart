60db888e7f2109231e3eddf301af2e8ab7379f02
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-24 09:08:26 +0700

    edit setting

--- lib/widgets/profiles/edit_profile.dart
+++ lib/widgets/profiles/edit_profile.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
 class DateOfBirth extends StatelessWidget {
+  final String label;
+  final String value;
+  
+  const DateOfBirth ({super.key, required this.label, required this.value});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Date of Birth'),),
+      appBar: AppBar(title: Text(label),),
       body: Center (child: Text('Details about Date of Birth'),),
+    );
+  }
+  
+}
+class Weight extends StatelessWidget {
+  final String label;
+  final String value;
+  
+  const Weight ({super.key, required this.label, required this.value});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(label),),
+      body: Center (child: Text('Details about Weight'),),
     );
   }
   
