195ff9e1b67f6b33fefb4b0f1d3318fa299de357
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-30 14:31:30 +0700

    feactch api

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@
 //   }
 // }
 
-
 import 'dart:convert';
 import 'package:Khmervie/test.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +24,7 @@ void main() => runApp(BookApp());
 
 class BookApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: BooksScreen(),
     );
@@ -33,27 +32,30 @@ class BookApp extends StatelessWidget {
 }
 
 class BooksScreen extends StatefulWidget {
-@override
-_BooksScreenState createState() => _BooksScreenState();
+  @override
+  _BooksScreenState createState() => _BooksScreenState();
 }
+
 class _BooksScreenState extends State<BooksScreen> {
   List<Book> _books = [];
   @override
   void initState() {
     super.initState();
-    _fetchBooks();
   }
+
   Future<void> _fetchBooks() async {
-    final response = await http.get(Uri.parse('http://192.168.195.138:3000/api/books'));
+    final response =
+        await http.get(Uri.parse('http://192.168.195.138:3000/api/books'));
     if (response.statusCode == 200) {
       final List<dynamic> json = jsonDecode(response.body);
       setState(() {
-      _books = json.map((item) => Book.fromJson(item)).toList();
+        _books = json.map((item) => Book.fromJson(item)).toList();
       });
     } else {
       throw Exception('Failed to load books');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,14 +63,13 @@ class _BooksScreenState extends State<BooksScreen> {
         title: Text('Books'),
       ),
       body: ListView.builder(
-        itemCount: _books.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(_books[index].title),
-            subtitle: Text(_books[index].author),
-          );
-        },
-      ),
+          itemCount: _books.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(_books[index].title),
+              subtitle: Text(_books[index].author),
+            );
+          }),
     );
   }
- }
+}
