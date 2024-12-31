a5310ab5eb2fc24573f8fb81af9a385fea4d45d7
Choun Piseth <chuonpiseth05@gmail.com>
2024-12-31 11:41:52 +0700

    register

--- lib/login/register.dart
+++ lib/login/register.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,6 +11,22 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _signup() async {
+    final response = await http.post(
+      Uri.parse('http://localhost:3000/signup'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'name': _nameController.text,
+        'email': _emailController.text,
+        'password': _passwordController,
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
