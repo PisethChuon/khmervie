650b63fc02ad8ed42303a0e5b617e42c47cb4c33
Choun Piseth <chuonpiseth05@gmail.com>
2025-01-02 13:55:24 +0700

    server

--- node-api/server.js
+++ node-api/server.js
@@ -19,10 +19,9 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
+    console.error("Error connecting to MySQL: ", err);
   }
-  console.log("Connected to MySQL!");
+  console.log("Connecte to MYSQL!");
 });
 
 // Signup Route
